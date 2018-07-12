37ffb0fdae7ed85ef028887853d7af3ccd05d6a5
Avivi, Yotam (ya107f) <ya107f@intl.att.com>
2018-07-12 14:11:07 +0300

    Initial Commit

---
+++ src/main/java/com/att/app/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.att.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+		return "Hello";
+	}
+
+}
+
---
+++ src/test/java/com/att/test/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.att.test.app;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+	@Before
+	public void before() throws Exception {
+		System.out.println("before");
+	}
+
+	@After
+	public void after() throws Exception {
+		System.out.println("after");
+	}
+	
+	@Test
+	public void test(){
+		assertThat(true).isEqualTo(true);		
+	}
+
+}
